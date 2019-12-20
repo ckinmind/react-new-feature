64a5ca8347a834531cd9035a310cb8f08c8421b7
ckinmind <ckinmind@126.com>
2019-12-20 11:41:39 +0800

    add useState example

--- stories/16.8.stories.js
+++ stories/16.8.stories.js
@@ -4,5 +4,8 @@ import {storiesOf} from '@storybook/react'
 
 import HKMD from '../src/16.8/hooks.md'
 
+import UseStateExample from '../src/16.8/hooks-useState'
+
 storiesOf('React 16.8', module)
-  .add('Hooks', () => <div />, {info: {text: HKMD, inline: false, source: false, propTables: null}})
+  .add('Hooks', () => <div />, {info: {text: HKMD, inline: true, source: false, propTables: null}})
+  .add('hooks-useState', () => <UseStateExample />, {info: {text: HKMD, inline: false, source: false, propTables: null}})
